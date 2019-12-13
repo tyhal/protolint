400cc166710a455b6c0fb01126c5d4d9d441d9fc
Tyler Hale <t.w.hale32@gmail.com>
2019-12-14 03:06:49 +1300

    Create basic interface to call directly as a library not from an exec

---
+++ api/protolint.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"errors"
+	"github.com/yoheimuta/protolint/internal/cmd"
+	"io"
+	"strconv"
+)
+
+// Lint allows tyhal to use this thing as a lib
+func Lint(file string,
+	fix bool,
+	stdout io.Writer,
+	stderr io.Writer) (err error) {
+	args := []string{"lint"}
+	if fix {
+		args = append(args, "-fix")
+	}
+	exit := cmd.Do(
+		append(args, file),
+		stdout,
+		stderr,
+	)
+	if exit != 0 {
+		err = errors.New("protolint returned: " + strconv.Itoa(int(exit)))
+	}
+	return
+}
